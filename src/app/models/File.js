e1d8edaca539586151597728d19bc4cbb1039b0c
Richard Yamamoto <rybolteri@gmail.com>
2019-08-28 10:36:27 -0300

    chore: Create-file migration

---
+++ src/app/models/File.js
@@ -0,0 +1,9 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+  static init(sequelize) {
+    super.init({});
+  }
+}
+
+export default File;
--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,17 @@ import './database';
 
 const routes = new Router();
 
+// Users
 routes.post('/users', UserController.store);
+
+// Session
 routes.post('/session', SessionController.store);
 
+// Middlewares
 routes.use(authMiddleware);
+
+// User in Session
 routes.put('/users', UserController.update);
+// # Upload file
+routes.put('/users/:userId/uploads', FileController.store);
 export default routes;
